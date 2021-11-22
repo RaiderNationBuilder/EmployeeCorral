5f14e365aed4335d08c375de9805b1b84d2d838b
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-11-21 18:59:57 -0600

    stuck on INSERT INTO

--- index.js
+++ index.js
@@ -25,15 +25,19 @@ function introPrompt() {
                 "view all employees",
                 "add a department",
                 "add a role",
-                "add an employee",
+                "add an employees",
                 "update an employee role"
             ]
         }
-    ]).then(function (answer) {        
+    ]).then(function (answer) {
         if (answer.menu === "view all departments") {
             getAllDepartments()
         } else if (answer.menu === "view all roles") {
             getAllRoles()
+        } else if (answer.menu === "view all employees") {
+            getAllEmployees()
+        } else if (answer.menu === "add a department") {
+            addDepartment()
         }
     })
 }
@@ -44,18 +48,43 @@ function getAllDepartments() {
     db.query(
         `SELECT * FROM department`,
         function (err, results) {
-            console.log("in function x2")
-            console.log("results", results)
             console.table(results)
         });
 }
+
+function addDepartment() {
+    inquirer.prompt(
+        {
+            type: "input",
+            name: "name",
+            message: "What department would you like to add?",
+        }
+    ).then(function (answer) {
+        console.log(answer.name)
+        db.query(
+            `INSERT INTO department (name) VALUES (${answer.name})`, function (err, result) {
+                if (err) {
+                    throw err
+                } else{
+                console.log("1 record inserted");
+                getAllDepartments();
+                }
+              });
+    });
+};
 
 function getAllRoles() {
     db.query(
         `SELECT * FROM roles`,
         function (err, results) {
-            console.log("in function x2")
-            console.log("results", results)
+            console.table(results)
+        });
+}
+
+function getAllEmployees() {
+    db.query(
+        `SELECT * FROM employee`,
+        function (err, results) {
             console.table(results)
         });
 }
